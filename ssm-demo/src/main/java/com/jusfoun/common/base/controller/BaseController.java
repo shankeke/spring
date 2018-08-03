05f232ae269770519b222b8771df4331738e462b
jusfoun <jusfoun@jusfoun-PC>
2018-08-03 14:17:57 +0800

    微调

--- ssm-demo/src/main/java/com/jusfoun/common/base/controller/BaseController.java
+++ ssm-demo/src/main/java/com/jusfoun/common/base/controller/BaseController.java
@@ -23,21 +23,21 @@ public interface BaseController<T> {
 	BaseService<T> getBaseService();
 
 	@ApiOperation(value = "保存数据", notes = "保存数据", hidden = false)
-	@RequestMapping(value = "save", method = { RequestMethod.POST })
+	@RequestMapping(value = "save", method = {RequestMethod.POST})
 	default BaseResponse<T> save(@ApiParam(value = "数据对象", required = true) @RequestBody T t) {
 		getBaseService().insertSelective(t);
 		return BaseResponse.success(t);
 	}
 
 	@ApiOperation(value = "批量保存数据", notes = "批量保存数据", hidden = false)
-	@RequestMapping(value = "saveList", method = { RequestMethod.POST })
+	@RequestMapping(value = "saveList", method = {RequestMethod.POST})
 	default BaseResponse<?> saveList(@ApiParam(value = "数据对象集合", required = true) @RequestBody List<T> list) {
 		getBaseService().insertListSelective(list);
 		return BaseResponse.success();
 	}
 
 	@ApiOperation(value = "查询数据详情", notes = "查询数据详情", hidden = false)
-	@RequestMapping(value = "info", method = { RequestMethod.POST, RequestMethod.GET })
+	@RequestMapping(value = "info", method = {RequestMethod.POST, RequestMethod.GET})
 	default BaseResponse<T> info(@ApiParam(value = "查询参数", required = true) @RequestBody T t) {
 		t = getBaseService().selectOne(t);
 		return BaseResponse.success(t);
