f0811a5563fe88cba681c4d2a760d8c485253f19
jusfoun <jusfoun@jusfoun-PC>
2018-11-26 09:24:51 +0800

    修改代码生成配置

--- sbm-demo/src/main/java/com/jusfoun/mapper/ds1/TTestMapper.java
+++
@@ -1,9 +0,0 @@
-package com.jusfoun.mapper.ds1;
-
-import com.jusfoun.common.mybatis.mapper.MyIdableMapper;
-import com.jusfoun.entity.TTest;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface TTestMapper extends MyIdableMapper<TTest> {
-}
